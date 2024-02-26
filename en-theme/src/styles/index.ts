679f76abd5f000b36c6798477856a27de08c0ac2
kangsan <junbuck01@naver.com>
2024-02-26 11:22:36 +0900

    theme setup start

---
+++ en-theme/src/styles/index.ts
@@ -0,0 +1,10 @@
+import { createStitches } from '@stitches/react';
+import { colors } from '../styles';
+
+export const { styled, theme, globalCss } = createStitches({
+
+});
+
+export const globalStyles = globalCss({
+
+});
