8424542fc860f6f59c55ad1d6f3414b753733faa
kang-san <junbuck01@naver.com>
2024-02-27 22:26:32 +0900

    theme prop

--- en-theme/src/helpers/props/prop-def.ts
+++ en-theme/src/helpers/props/prop-def.ts
@@ -1,5 +1,4 @@
 import React from 'react';
-import type { Responsive } from '../breakpoints';
 
 type BooleanPropDef = { type: 'boolean'; default?: boolean; required?: boolean };
 type StringPropDef = { type: 'string'; default?: string; required?: boolean };
@@ -20,17 +19,5 @@ type RegularPropDef<T> =
 type ResponsivePropDef<T = any> = RegularPropDef<T> & { responsive: true };
 type PropDef<T = any> = RegularPropDef<T> | ResponsivePropDef<T>;
 
-// prettier-ignore
-type GetPropDefType<Def> =
-	  Def extends BooleanPropDef ? (Def extends ResponsivePropDef ? Responsive<boolean> : boolean)
-  : Def extends StringPropDef ? (Def extends ResponsivePropDef ? Responsive<string> : string)
-  : Def extends StringOrNumberPropDef ? (Def extends ResponsivePropDef ? Responsive<string | number> : string | number)
-  : Def extends ReactNodePropDef ? (Def extends ResponsivePropDef ? Responsive<React.ReactNode> : React.ReactNode)
-  : Def extends EnumPropDef<infer Type> ? (Def extends ResponsivePropDef<infer Type> ? Responsive<Type> : Type)
-  : never;
 
-type GetPropDefTypes<P> = {
-  [K in keyof P]?: GetPropDefType<P[K]>;
-};
-
-export type { PropDef, GetPropDefTypes };
+export type { PropDef };
